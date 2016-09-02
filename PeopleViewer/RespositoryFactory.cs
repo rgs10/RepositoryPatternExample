2adb9e07cd48cc095f412458044b947f5333784a
SmithR <SmithR@LAPTOP-RSMITH>
2016-09-02 14:33:44 +0100

    Add project files.

---
+++ PeopleViewer/RespositoryFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonRepository.CSV;
+using PersonRepository.Interface;
+using PersonRepository.Service;
+using PersonRepository.SQL;
+
+namespace PeopleViewer
+{
+    public static class RespositoryFactory
+    {
+        public static IPersonRepository GetRepository(string repositoryType)
+        {
+            IPersonRepository repo = null;
+            switch (repositoryType)
+            {
+                case "Service": repo = new ServiceRepository();
+                    break;
+                case "CSV": repo = new CSVRepository();
+                    break;
+                case "SQL": repo = new SQLRepository();
+                    break;
+                default:
+                    throw new ArgumentException("Invalid Repostiory type.");
+            }
+            return repo;
+        }
+    }
+}
---
+++ PersonRepository.Service/ServiceRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PersonRepository.Interface;
+using PersonRepository.Service.MyService;
+
+namespace PersonRepository.Service
+{
+    public class ServiceRepository : IPersonRepository
+    {
+        PersonServiceClient serviceProxy = new PersonServiceClient();  //wcf service reference
+
+        public IEnumerable<Person> GetPeople()
+        {
+            return serviceProxy.GetPeople();
+        }
+
+        public Person GetPerson(string lastName)
+        {
+            return serviceProxy.GetPerson(lastName);
+        }
+
+        public void AddPerson(Person newPerson)
+        {
+            serviceProxy.AddPerson(newPerson);
+        }
+
+        public void UpdatePerson(string lastName, Person updatedPerson)
+        {
+            serviceProxy.UpdatePerson(lastName, updatedPerson);
+        }
+
+        public void DeletePerson(string lastName)
+        {
+            serviceProxy.DeletePerson(lastName);
+        }
+
+
+        public void UpdatePeople(IEnumerable<Person> updatedPeople)
+        {
+            serviceProxy.UpdatePeople(updatedPeople.ToList());
+        }
+    }
+}
